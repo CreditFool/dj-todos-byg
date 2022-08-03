b27c56cf8d5b7f5284e403de187f0a28d5bc95d1
creditFool <bichlasulbulqiah@gmail.com>
2022-08-03 18:50:05 +0700

    db fix

--- todolistApp/settings.py
+++ todolistApp/settings.py
@@ -74,8 +74,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-if os.environ.get('DATABASE_URL'):
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
